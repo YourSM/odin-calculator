a131ec477634c6814185c593abb2a63d5f04fd64
YourSM <156563558+YourSM@users.noreply.github.com>
2024-08-06 12:21:13 -0700

    Add logic to calculate inputs and display them

--- javascript.js
+++ javascript.js
@@ -1,16 +1,32 @@
 let numOne = 0;
 let numTwo = 0;
-let operator = 0;
+let operatorSymbol = 0;
 
 const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button")
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
 
-buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
     let target = event.target.textContent
     display.append(target);
+    return operatorSymbol === 0 ? numOne = +target : numTwo = +target
   })
 })
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    let target = event.target.textContent;
+    if (target === "=") {
+      let result = (operate(numOne, operatorSymbol, numTwo))
+      display.textContent = result
+      return numOne = result
+    } else {
+      display.append(target);
+      return operatorSymbol = target;
+    }
+    })
+  })
 
 function add(numOne, numTwo) {
   return numOne + numTwo;
@@ -28,14 +44,14 @@ function divide(numOne, numTwo) {
   return numOne / numTwo;
 }
 
-function operate(numOne, operator, numTwo) {
-  if (operator === "+") {
+function operate(numOne, operatorSymbol, numTwo) {
+  if (operatorSymbol === "+") {
     return add(numOne, numTwo)
-  } else if (operator === "-") {
+  } else if (operatorSymbol === "-") {
     return subtract(numOne, numTwo)
-  } else if (operator === "*") {
+  } else if (operatorSymbol === "*") {
     return multiply(numOne, numTwo);
-  } else if (operator === "/") {
+  } else if (operatorSymbol === "/") {
     return divide(numOne, numTwo)
   }
 }
